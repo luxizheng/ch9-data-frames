c0e018bbad276038adb1d56396bc4cb17115b28e
luxizheng <lucyzheng09@gmail.com>
2018-01-25 10:43:19 -0800

    Finish exercises

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -2,28 +2,29 @@
 
 # Create a vector of 100 employees ("Employee 1", "Employee 2", ... "Employee 100")
 # Hint: use the `paste()` function and vector recycling
+employees <- c(paste("Employee", 1:100)) 
 
 
 # Create a vector of 100 random salaries for the year 2016
 # Use the `runif()` function to pick random numbers between 40000 and 50000
-
+salaries.2016 <- runif(100, 40000, 50000)
 
 # Create a vector of 100 salaries in 2017 that have changed from 2016
 # Use `runif()` to add a random number between -5000 and 10000 to each of 2016's
 # salaries (the negative number means that a salary may have decreased!)
-
+salaries.2017 <- salaries.2016 + runif(100, -5000, 10000)
 
 # Create a data frame 'salaries' by combining the 3 vectors you just made
 # Remember to set `stringsAsFactors=FALSE`!
-
+salaries <- data.frame(employees, salaries.2016, salaries.2017, stringsAsFactors = FALSE)
 
 # Create a column 'change' that stores each person's change in salary between
 # 2016 and 2017
-
+salaries$change <- salaries$salaries.2017 - salaries$salaries.2016
 
 # Create a column 'got.raise' that is TRUE if the person got a raise (their
 # salary went up)
-
+salaries$got.raise <- salaries$change > 0
 
 
 ### Retrieve values from your data frame to answer the following questions
@@ -31,16 +32,17 @@
 ### cell rather than the whole row!)
 
 # What was the 2017 salary of employee 57
-
+salaries[57, "salaries.2017"]
 
 # How many employees got a raise?
-
+got.raise.ppl <- salaries$got.raise[salaries$got.raise == TRUE]
+length(got.raise.ppl)
 
 # What was the dollar value of the highest raise?
-
+max(salaries$change)
 
 # What was the "name" of the employee who received the highest raise?
-
+salaries[salaries$change == max(salaries$change), ]
 
 # What was the largest decrease in salaries between the two years?
 
